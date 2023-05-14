51134e09752c54aa097af209bdeae317269d6d17
Gyanendra Shukla <srirajshukla@gmail.com>
2023-05-14 08:44:31 +0530

    feat: basic argument parsing

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,2 +1,20 @@
+import argparse
+
 def main():
-    print("ugit")
+    args = parse_args()
+    args.func(args)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init():
+    print('ugit')
