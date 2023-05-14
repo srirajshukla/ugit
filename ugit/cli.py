48f0cb81a1f6743adb1ce8873037515d4f86dcac
Gyanendra Shukla <srirajshukla@gmail.com>
2023-05-14 09:08:29 +0530

    feat: add object hash

--- ugit/cli.py
+++ ugit/cli.py
@@ -18,6 +18,10 @@ def parse_args():
     init_parser = commands.add_parser("init")
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser("hash-object")
+    hash_object_parser.add_argument("file")
+    hash_object_parser.set_defaults(func=hash_object)
+
     return parser.parse_args()
 
 
@@ -27,3 +31,8 @@ def init(args):
         print(f"Initialized empty ugit repository in {os.getcwd()}\{data.GIT_DIR}")
     else:
         print(f"Ugit repo already exists at {os.getcwd()}")
+
+
+def hash_object(args):
+    with open(args.file, "rb") as f:
+        print(data.hash_object(f.read()))
--- ugit/data.py
+++ ugit/data.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 GIT_DIR = ".ugit"
 
@@ -6,6 +7,14 @@ GIT_DIR = ".ugit"
 def init():
     try:
         os.makedirs(GIT_DIR)
+        os.makedirs(f"{GIT_DIR}/objects")
         return True
     except OSError as e:
         return False
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f"{GIT_DIR}/objects/{oid}", "wb") as f:
+        f.write(data)
+    return oid
