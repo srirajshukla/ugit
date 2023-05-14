63aa403cee01b00067436fab452e1c75c9c925bd
Gyanendra Shukla <srirajshukla@gmail.com>
2023-05-14 08:54:27 +0530

    style: reformat with black

--- ugit/cli.py
+++ ugit/cli.py
@@ -3,17 +3,19 @@ import os
 
 from . import data
 
+
 def main():
     args = parse_args()
     args.func(args)
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    commands = parser.add_subparsers(dest='command')
+    commands = parser.add_subparsers(dest="command")
     commands.required = True
 
-    init_parser = commands.add_parser('init')
+    init_parser = commands.add_parser("init")
     init_parser.set_defaults(func=init)
 
     return parser.parse_args()
