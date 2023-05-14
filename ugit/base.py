ea1dbcf2c6bbbfee8fd1771111b1805f4913cd56
Gyanendra Shukla <srirajshukla@gmail.com>
2023-05-14 10:11:41 +0530

    "fix: dont add ignored directories"

--- ugit/base.py
+++ ugit/base.py
@@ -6,9 +6,20 @@ from . import data
 def write_tree(directory="."):
     for entry in pathlib.Path(directory).iterdir():
         if entry.is_file() and not entry.is_symlink():
+            if is_ignored(entry):
+                continue
             print(entry)
             # TODO: create the oid by writing to object store
         elif entry.is_dir() and not entry.is_symlink():
             write_tree(entry)
 
     # TODO: create the object tree
+
+
+def is_ignored(path):
+    ignore_dirs = [".git", ".ugit", "ugit.egg-info", "__pycache__"]
+    path_parts = pathlib.Path(path).parts
+    for igd in ignore_dirs:
+        if igd in path_parts:
+            return True
+    return False
