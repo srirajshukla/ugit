191ced797cbf8069952d20746ea02f51533d0451
Gyanendra Shukla <srirajshukla@gmail.com>
2023-05-14 11:37:19 +0530

    feat: read files from object storage and write to directory

--- ugit/base.py
+++ ugit/base.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 
 from . import data
@@ -32,3 +33,36 @@ def is_ignored(path):
         if igd in path_parts:
             return True
     return False
+
+
+def _iter_tree_entries(oid):
+    if not oid:
+        return
+    tree = data.get_object(oid, "tree")
+    for entry in tree.decode().splitlines():
+        type_, oid, name = entry.split(" ", 2)
+        yield (type_, oid, name)
+
+
+def get_tree(oid, base_path=""):
+    result = {}
+    for type_, oid, name in _iter_tree_entries(oid):
+        if name in ["/", ".", ".."]:
+            raise AssertionError(f"/, ., .., in path {name}")
+
+        path = pathlib.Path(base_path, name)
+
+        if type_ == "blob":
+            result[path] = oid
+        elif type_ == "tree":
+            result.update(get_tree(oid, f"{path}/"))
+        else:
+            raise ValueError(f"Unknown type {type_}")
+    return result
+
+
+def read_tree(tree_oid):
+    for path, oid in get_tree(tree_oid, base_path="./").items():
+        os.makedirs(path, exist_ok=True)
+        with open(path, "wb") as f:
+            f.write(data.get_object(oid=oid))
--- ugit/cli.py
+++ ugit/cli.py
@@ -30,6 +30,10 @@ def parse_args():
     write_tree_parser = commands.add_parser("write-tree")
     write_tree_parser.set_defaults(func=write_tree)
 
+    read_tree_parser = commands.add_parser("read-tree")
+    read_tree_parser.add_argument("tree")
+    read_tree_parser.set_defaults(func=read_tree)
+
     return parser.parse_args()
 
 
@@ -52,4 +56,8 @@ def cat_file(args):
 
 
 def write_tree(args):
-    base.write_tree()
+    print(base.write_tree())
+
+
+def read_tree(args):
+    base.read_tree(args.tree)
