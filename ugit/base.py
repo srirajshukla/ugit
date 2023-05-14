6457587e86da1a4db1ffe04b99ce62ef87dc8ed9
Gyanendra Shukla <srirajshukla@gmail.com>
2023-05-14 10:57:01 +0530

    fix: create object tree and write data to files

--- ugit/base.py
+++ ugit/base.py
@@ -4,16 +4,25 @@ from . import data
 
 
 def write_tree(directory="."):
+    entries = []
     for entry in pathlib.Path(directory).iterdir():
+        if is_ignored(entry):
+            continue
         if entry.is_file() and not entry.is_symlink():
-            if is_ignored(entry):
-                continue
-            print(entry)
-            # TODO: create the oid by writing to object store
-        elif entry.is_dir() and not entry.is_symlink():
-            write_tree(entry)
+            type_ = "blob"
+            with open(entry, "rb") as f:
+                oid = data.hash_object(f.read())
 
-    # TODO: create the object tree
+        elif entry.is_dir() and not entry.is_symlink():
+            type_ = "tree"
+            oid = write_tree(entry)
+
+        entries.append((entry, oid, type_))
+
+    tree = "".join(f"{type_} {oid} {name}\n" for name, oid, type_ in sorted(entries))
+    print(tree)
+
+    return data.hash_object(tree.encode(), "tree")
 
 
 def is_ignored(path):
